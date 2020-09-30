e099bedbd257101d7ca017a73a2536dcf50b2f27
19tiWuTan <64946416+19tiWuTan@users.noreply.github.com>
2020-09-30 14:22:21 +0700

    Update killmyheart_290920.cpp

--- killmyheart_290920.cpp
+++ killmyheart_290920.cpp
@@ -1,72 +1,80 @@
 // task : https://codeforces.com/contest/1396/problem/C
-#include<bits/stdc++.h>
-//#pragma GCC optimize("Ofast")
+// i'm wutan
+#include <bits/stdc++.h>
+#define io_faster ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define EL cout<<'\n'
+#define pli pair<ll,int>
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define fi first
+#define se second
+#define sz(a) int(a.size())
+#define FU(x,a,b) for(int x=int(a);x<=int(b);x++)
+#define FD(x,a,b) for(int x=int(a);x>=int(b);x--)
+#define PROB "killmyheart"
 
 using namespace std;
+typedef long long ll;
+typedef double db;
 
-#define X first
-#define Y second
-#define ll long long
-#define ld double
-#define up_b upper_bound
-#define low_b lower_bound
-#define pb push_back
-#define mp make_pair
-
-typedef pair<int,int> pii;
-
-const long long mod=1e9+7;
-const int N=1e6;
-
-
-pii origin;
-void operator-=(pii &A, pii B) {
-    A.X -= B.X;
-    A.Y -= B.Y;
+template <typename T>
+inline void read(T& x){
+    bool Neg = false;
+    char c;
+    for (c = getchar(); c < '0' | c > '9'; c = getchar())
+        if (c == '-') Neg = !Neg;
+    x = c - '0';
+    for (c = getchar(); c >= '0' && c <= '9'; c = getchar())
+        x = x * 10 + c - '0';
+    if (Neg) x = -x;
 }
-bool ccw(pii O, pii A, pii B) {
-    A -= O, B -= O;
-    return A.X * B.Y > A.Y * B.X;
-}
-bool cmp(pii A, pii B) { return ccw(origin, A, B); }
 
-ll Power(int x,int y)
-{
-    ll ans=1;
-    for (ll t=x;y;y>>=1)
-    {
-        if (y&1) ans=(ans*t);
-        t=(t*t);
+template <typename T>
+inline void write(T x){
+    if (x < 0)
+        putchar('-'); x = -x;
+    T p = 1;
+    for (T temp = x / 10; temp > 0; temp /= 10) p *= 10;
+    for (; p > 0; x %= p, p /= 10) putchar(x / p + '0');
+}
+
+void file(){
+#ifndef ONLINE_JUDGE
+	freopen(PROB".inp","r",stdin);
+	freopen(PROB".out","w",stdout);
+#endif
+}
+
+const int N = 1e6 + 1;
+int n; ll a[N], r1, r2, r3, d;
+ll dp[N][2];
+void readinp(){
+    cin >> n >> r1 >> r2 >> r3 >> d;
+    FU(i, 1, n) cin >> a[i];
+}
+
+void solve(){
+    dp[0][1] = 0;
+    dp[0][0] = -d;
+    FU(i, 1, n){
+        dp[i][1] = min(dp[i-1][0] + d + min(r2, (a[i] + 1) * r1),
+                       dp[i-1][1] + d + min(r2, (a[i] + 1) * r1) + d + r1 + d);
+        dp[i][0] = min(dp[i-1][0] + d + a[i] * r1 + r3,
+                       dp[i-1][1] + d + min(r2, (a[i] + 1) * r1) + d + r1 + d + r1);
     }
-    return ans;
+    cout << min(dp[n][0], dp[n-1][1] + d + a[n] * r1 + r3 + d + r1);
 }
-int Rev(int x)
-{
-    return Power(x,mod-2);
-}
-long long dp[N+5][2];
-long long n,r1,r2,r3,d;
-long long a[N+5];
-int solve()
-{
-    cin>>n>>r1>>r2>>r3>>d;
-    for (int i=1;i<=n;i++) cin>>a[i];
-    dp[0][0]=-d;
-    dp[0][1]=mod;
-    for (int i=1;i<=n;i++)
-    {
-        dp[i][1]=min(dp[i-1][0]+d+min(r2,(a[i]+1)*r1),dp[i-1][1]+d+min(r2,(a[i]+1)*r1)+d+r1+d);
-        dp[i][0]=min(dp[i-1][0]+d+a[i]*r1+r3,dp[i-1][1]+d+min(r2,(a[i]+1)*r1)+d+r1+d+r1);
-    }
 
-//    for (int i=1;i<=n;i++) cout<<dp[i][0]<<' '<<dp[i][1]<<"\n";
-    cout<<min(dp[n][0],dp[n-1][1]+d+a[n]*r1+r3+d+r1);
+int main(){
+    io_faster
+    file();
+    int t = 1;
+//    cin >> t;
+    while (t--){
+        readinp();
+        solve();
+    }
 }
-int main()
-{
-  //  freopen("cf.inp","r",stdin);
-  //  freopen("cf.out","w",stdout);
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    solve();
-}
+
+
+
