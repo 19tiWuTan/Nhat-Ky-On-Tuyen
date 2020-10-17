2a4827f71618b61cffd9bf4a2e88b2eb005a9e2a
19tiWuTan <64946416+19tiWuTan@users.noreply.github.com>
2020-10-17 11:09:07 +0700

    Create 201017_CommemorativeRace.cpp

---
+++ 201017_CommemorativeRace.cpp
@@ -0,0 +1,14 @@
+/// task : cho đồ thị DAG, tìm 1 cạnh (u -> v) sao cho tất cả đường đi dài nhất đều đi qua cạnh này và đường đi dài nhất đi qua u nhưng không đi qua (u -> v) là  nhỏ nhất
+/// trước hết xác định tất cả các đường đi dài nhất là duy nhất ( bằng BFS ) thì tát cả đường đi dài nhất đều đi qua những cạnh này
+
+/// gọi dp[u] sẽ là dường đi dài nhất bắt đầu từ u, và  dp2[u] sẽ là đường đi dài thứ nhì bắt đầu từ u
+/// ta tạo 1 đỉnh ảo p = 0, nối p với mọi đỉnh i (i = 1.. n)
+/// gọi dfs(0) để tạo dp[u] và dp2[u]
+
+/// để xác định xét cạnh (u->v) có phải là cạnh duy nhất hay không : 
+/// ta sẽ loang từ đỉnh u (u chắc chắn nằm trên đường đi dài nhất) ra đỉnh (v sao cho (u->v) thuộc đường đi dài nhất xuất phát từ u)
+/// đánh số các lớp theo độ sâu khi bfs, cạnh (u->v) là duy nhất nếu lớp mà đỉnh (u) thuộc, chỉ có duy nhất đỉnh u
+
+
+///
+
