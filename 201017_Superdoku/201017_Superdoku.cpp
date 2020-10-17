09565b0c52f2dc89cd0e3d6f96019b480d5b7116
19tiWuTan <64946416+19tiWuTan@users.noreply.github.com>
2020-10-17 16:41:46 +0700

    Create 201017_Superdoku.cpp

---
+++ 201017_Superdoku/201017_Superdoku.cpp
@@ -0,0 +1,17 @@
+/*
+ví dụ :
+4 2 
+1 2 3 4
+2 3 4 1
+ở cột 1 có thể điền giá trị 3, 4
+ở cột 2 có thể điền giá trị 1, 4
+ở cột 3 có thể diền giá trị 1, 2
+ở cột 4 có thể điền giá trị 2, 3
+ta có đồ thị 2 phía với các cạnh trên : 
+  cột X   |   giá trị Y
+    1            1
+    2            2 
+    3            3
+    4            4 
+    ta sẽ tìm cặp ghép cực đại trên đồ thị 2 phía này
+*/
