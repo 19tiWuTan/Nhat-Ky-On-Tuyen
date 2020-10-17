3c0255aaf0ff776347fad6dbe94584129b5dfc1c
19tiWuTan <64946416+19tiWuTan@users.noreply.github.com>
2020-10-17 13:08:29 +0700

    Update 201017_LargestQuadrilateral.cpp

--- 201017_LargestQuadrilateral.cpp
+++ 201017_LargestQuadrilateral.cpp
@@ -15,112 +15,133 @@
 // với m > 3
 // gọi f1[i][j] sẽ là diện tích của tam giác có diện tích lớn nhất có đáy là a[i] và a[j], đỉnh còn lại nằm trên đường thẳng nối (a[i], a[j])
 // gọi f2[i][j] sẽ là diện tích của tam giác có diện tích lớn nhất có đáy là a[i] và a[j], đỉnh còn lại nằm dưới đường thẳng nối (a[i], a[j])
+
+// i'm wutan
 #include <bits/stdc++.h>
+#define io_faster ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define EL cout<<'\n'
+#define pli pair<ll,int>
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define fi first
+#define se second
+#define sz(a) int(a.size())
+#define FU(x,a,b) for(int x=int(a);x<=int(b);x++)
+#define FD(x,a,b) for(int x=int(a);x>=int(b);x--)
+#define PROB "LargestQuadrilateral"
+
 using namespace std;
+typedef long long ll;
+typedef double db;
 
-#define ll long long
-typedef pair<ll, ll> point;
-#define x first
-#define y second
-const int N = 4096;
-point a[N + 1], st[N + 1], hull[N + 1];
-ll ftop[N + 1][N + 1], fbot[N + 1][N + 1];
-int n, m;
-
-ll stri(point a, point b, point c){
-    ll tmp1 = a.x * (c.y - b.y);
-    ll tmp2 = b.x * (a.y - c.y);
-    ll tmp3 = c.x * (b.y - a.y);
-    return (tmp1 + tmp2 + tmp3);
+void file(){
+#ifndef ONLINE_JUDGE
+    freopen(PROB".inp","r",stdin);
+    freopen(PROB".out","w",stdout);
+#endif
 }
 
-ll S(point a, point b, point c){
-    return abs(stri(a, b, c));
+const int N = 5005;
+int n; pll b[N], a[N];
+pll st[N * 2], sd[N]; int topt, topd, in[N];
+ll f[2][N][N];
+vector < pll > hull;
+
+void readinp(){
+    cin >> n;
+    FU(i, 1, n) cin >> a[i].fi >> a[i].se;
 }
 
-bool directop(point a, point b, point c){
-    return (stri(a, b, c) >= 0);
+ll Stri(pll t1, pll t2, pll t3){
+    ll s1 = t1.fi * (t3.se - t2.se);
+    ll s2 = t2.fi * (t1.se - t3.se);
+    ll s3 = t3.fi * (t2.se - t1.se);
+    return (s1 + s2 + s3);
 }
 
-bool direcbot(point a, point b, point c){
-    return (stri(a, b, c) < 0);
+bool check (int c, pll t1, pll t2, pll t3){
+    ll s = Stri(t1, t2, t3);
+    if (c == 1) return (s >= 0);
+    if (c == 0) return (s < 0);
 }
 
 void convexhull(){
-    sort(a + 1, a + n + 1);
-    int top = 1;
-    st[top] = a[1];
-    for(int i = 2; i <= n; ++i){
-        while(top >= 2 && !(directop(st[top - 1], st[top], a[i]))) --top;
-        st[++top] = a[i];
+    topt = topd = 0;
+    FU(i, 1, n){
+        while (topt >= 2 && !check(1, st[topt-1], st[topt], a[i]))
+            topt--;
+        while (topd >= 2 && !check(0, sd[topd-1], sd[topd], a[i]))
+            topd--;
+        st[++topt] = sd[++topd] = a[i];
     }
-    m = 0;
-    for(int i = 1; i <= top; ++i){
-        hull[m] = st[i];
-        ++m;
+    FD(i, topd - 1, 2)
+        st[++topt] = sd[i];
+    hull.clear();
+    FU(i, 1, topt) hull.push_back(st[i]);
+}
+
+ll S(pll t1, pll t2, pll t3){
+    return abs(Stri(t1, t2, t3));
+}
+
+int nxt(int i, int add, int m){
+    int ret = (i + add) % m;
+    return (ret ? ret : m);
+}
+
+void solve(){
+    FU(i, 1, n) in[i] = 0;
+    sort(a + 1, a + 1 + n);
+
+    convexhull();
+    int m = sz(hull);
+    ll res = 0;
+    if (m <= 2) res = 0LL;
+    else if (m == 3){
+        FU(i, 1, n) if(a[i] != hull[0] && a[i] != hull[1] && a[i] != hull[2]){
+            ll tmp = -1;
+            tmp = max(tmp, S(hull[0], hull[1], a[i]) + S(hull[0], hull[2], a[i]));
+            tmp = max(tmp, S(hull[1], hull[2], a[i]) + S(hull[1], hull[0], a[i]));
+            tmp = max(tmp, S(hull[2], hull[1], a[i]) + S(hull[2], hull[0], a[i]));
+            res = max(res, tmp);
+        }
     }
-    top = 1;
-    st[top] = a[1];
-    for(int i = 2; i <= n; ++i){
-        while(top >= 2 && !(direcbot(st[top - 1], st[top], a[i]))) --top;
-        st[++top] = a[i];
+    else{
+        FU(i, 0, m - 1) FU(j, 0, m - 1) FU(k, 0, 1) f[k][i][j] = 0;
+
+        FU(i, 0, m - 1){
+            int L = i;
+            for(int j = (i + 1) % m; j != i % m; j = (j + 1) % m){
+                int nextL = (L + 1) % m;
+                if(S(hull[i], hull[j], hull[L]) < S(hull[i], hull[j], hull[nextL])) L = nextL;
+                f[0][i][j] = f[0][j][i] = S(hull[i], hull[j], hull[L]);
+            }
+        }
+
+        FU(i, 0, m - 1){
+            int L = i;
+            for(int j = (i - 1 + m) % m; j != i % m; j = (j - 1 + m) % m){
+            int nextL = (L - 1 + m) % m;
+            if(S(hull[i], hull[j], hull[L]) < S(hull[i], hull[j], hull[nextL])) L = nextL;
+                f[1][i][j] = f[1][j][i] = S(hull[i], hull[j], hull[L]);
+            }
+        }
+        FU(i, 0, m - 1)
+        FU(j, 0, m - 1)
+            res = max(res, f[0][i][j] + f[1][i][j]);
+
     }
-    for(int i = top - 1; i > 1; --i){
-        hull[m] = st[i];
-        ++m;
-    }
+    if (res < 0) cout << 0;
+    else cout << res / 2LL << (res % 2LL ? ".5" : ""); EL;
 }
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int t; cin >> t;
-    while(t--){
-        cin >> n;
-        for(int i = 1; i <= n; ++i){
-            cin >> a[i].x >> a[i].y;
-        }
-        convexhull();
-        ll ans = 0;
-        if(m == 3){
-            for(int i = 1; i <= n; ++i){
-                if(a[i] != hull[0] && a[i] != hull[1] && a[i] != hull[2]){
-                    ll tmp = S(hull[0], hull[1], a[i])
-                                + S(hull[0], hull[2], a[i]);
-                    tmp = max(tmp, S(hull[1], hull[2], a[i])
-                                + S(hull[1], hull[0], a[i]));
-                    tmp = max(tmp, S(hull[2], hull[1], a[i])
-                                + S(hull[2], hull[0], a[i]));
-                    ans = max(ans, tmp);
-                }
-            }
-        }
-        if(m >= 4){
-            for(int i = 0; i < m; ++i){
-                int L = i;
-                for(int j = (i + 1) % m; j != i % m; j = (j + 1) % m){
-                    int nextL = (L + 1) % m;
-                    if(S(hull[i], hull[j], hull[L]) < S(hull[i], hull[j], hull[nextL])) L = nextL;
-                    ftop[i][j] = ftop[j][i] = S(hull[i], hull[j], hull[L]);
-                }
-            }
-            for(int i = 0; i < m; ++i){
-                int L = i;
-                for(int j = (i - 1 + m) % m; j != i % m; j = (j - 1 + m) % m){
-                    int nextL = (L - 1 + m) % m;
-                    if(S(hull[i], hull[j], hull[L]) < S(hull[i], hull[j], hull[nextL])) L= nextL;
-                    fbot[i][j] = fbot[j][i] = S(hull[i], hull[j], hull[L]);
-                }
-            }
-            for(int i = 0; i < m; ++i){
-                for(int j = 0; j < m; ++j){
-                    ans = max(ans, fbot[i][j] + ftop[i][j]);
-                }
-            }
-        }
-        cout << ans / 2;
-        if(ans % 2) cout << ".5";
-        cout << "\n";
+    io_faster
+    file();
+    int t = 1;
+    cin >> t;
+    while (t--){
+        readinp();
+        solve();
     }
-    return 0;
 }
