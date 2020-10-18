b825913a4afde3f867ade24e0f76bd8723b2ae03
19tiWuTan <64946416+19tiWuTan@users.noreply.github.com>
2020-10-18 11:38:53 +0700

    Create 201018_GCD.cpp

---
+++ 201018_GCD.cpp
@@ -0,0 +1,10 @@
+/*
+  task : cho dãy N số nguyên A và M số nguyen B
+  tìm cách loại bỏ ít nhất phần tử từ dãy A hoặc dãy B sao cho không tồn tại cặp (i, j) (1 <= i <= N, 1 <= j <= M) sao cho gcd(a[i], b[j]) > 1
+  
+  với những cặp A[i], B[j] nào có gcd > 1, thì ta dựng cạnh (i, j) 
+  -> đồ thị hai phía
+  -> kiến thức : tập phủ đỉnh trong đồ thị hai phía
+   
+  
+*/
