827f1aca10e7fa046192cf9e566108ef534c294e
19tiWuTan <64946416+19tiWuTan@users.noreply.github.com>
2020-10-18 16:20:59 +0700

    Create 201018_JumpingPath.cpp

---
+++ 201018_JumpingPath.cpp
@@ -0,0 +1,8 @@
+/*
+    cho 1 cái cây N đỉnh , mỗi đỉnh 1 giá trị
+    đếm số dãy con tăng dài nhất trên 1 cái cây : x[ui] <= x[u[i+1]] <= ... x[u[i+k-1]] sao cho u[i] là cha của u[i+1] và u[i+1] là cha của u[i+1] and so on
+    
+    
+    
+
+*/
