03abfcd853bedb1f100466e24261cff0aa4709ee
19tiWuTan <64946416+19tiWuTan@users.noreply.github.com>
2020-10-18 18:50:23 +0700

    Update 201018_Pokegene.cpp

--- 201018_Pokegene.cpp
+++ 201018_Pokegene.cpp
@@ -50,103 +50,95 @@
 
 */
 
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#define _USE_MATH_DEFINES
+// i'm wutan
 #include <bits/stdc++.h>
-
+#define io_faster ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define EL cout<<'\n'
+#define pli pair<ll,int>
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define pb(x) push_back(x)
+#define fi first
+#define se second
+#define sz(a) int(a.size())
+#define FU(x,a,b) for(int x=int(a);x<=int(b);x++)
+#define FD(x,a,b) for(int x=int(a);x>=int(b);x--)
+#define PROB "Pokegene"
 using namespace std;
+typedef long long ll;
+typedef double db;
 
-#define INP "solve"
-#define OUT "solve"
-
-const long long INF_LL = 1e17;
-const int INF = 1e9 + 100;
-const int MOD = 1e9 + 7;
-const int Base = 311;
-const double EPS = 1e-9;
-const int BLOCK = 1000;
-const int dx[6] = {0, 0, 1, -1, 1, -1};
-const int dy[6] = {1, -1, 0, 0, 1, -1};
-
-void open_file() {
-    #ifdef THEMIS
-        freopen(INP ".inp","r",stdin);
-        freopen(OUT ".out","w",stdout);
-    #endif // THEMIS
+const int N = 2e5 + 2;
+const int base = 331;
+const ll mod = 1e9 + 7;
+int n, q, p[N], id[N];
+string s[N];
+vector < vector < ll > > has;
+bool cmp (int t1, int t2){
+    return s[t1] < s[t2];
 }
 
-const int maxN = 5e3 + 10;
+bool cmp_pos(int t1, int t2){
+    return id[t1] < id[t2];
+}
 
-void sol() {
-    int n, q;
+int LCP(int i, int j){
+    int l = 1, r = min((int)s[i].size(), (int)s[j].size());
+    int ret = 0;
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (has[i][mid] == has[j][mid]) {
+            ret = mid;
+            l = mid + 1;
+        }
+        else r = mid - 1;
+    }
+    return ret;
+}
+
+void solve(){
     cin >> n >> q;
-    vector<vector<int> > h(n);
-    vector<string> s(n);
-    vector<int> p(n), id(n);
-    for (int i = 0; i < n; i++) {
+    has.resize(n + 2);
+    FU(i, 0, n - 1){
         cin >> s[i];
-        int sz = (int)s[i].size();
-        h[i].resize(sz + 1);
-        for (int j = 1; j <= sz; j++) {
-            h[i][j] = (1LL * h[i][j - 1] * Base + (int)s[i][j - 1]) % MOD;
-        }
         p[i] = i;
+        int siz = s[i].size();
+        has[i].resize(siz + 1);
+        has[i][0] = 0;
+        FU(j, 1, s[i].size())
+            has[i][j] = (has[i][j-1] * base + s[i][j-1] - 'a' + 1) % mod;
     }
-    auto LCP = [&](int i, int j) {
-        int L = 1, R = min((int)s[i].size(), (int)s[j].size());
+
+    sort(p, p + n, cmp);
+    FU(i, 0, n - 1) id[p[i]] = i;
+
+    while (q--){
+        int k, l;
+        cin >> k >> l;
+        vector < int > pos(k);
+        FU(i, 0, k - 1){
+            cin >> pos[i];
+            pos[i]--;
+        }
+        sort(pos.begin(), pos.end(), cmp_pos);
         int res = 0;
-        while (L <= R) {
-            int mid = (L + R) >> 1;
-            if (h[i][mid] == h[j][mid]) {
-                res = mid;
-                L = mid + 1;
-            }
-            else R = mid - 1;
+        FU(i, 0, k - l){
+            int add = LCP(pos[i], pos[i+l-1]);
+            res+= add - min(add, max(i ? LCP(pos[i], pos[i-1]) : 0
+                                   , i + l < k ? LCP(pos[i], pos[i+l]) : 0));
         }
-        return res;
-    };
-    sort(p.begin(), p.end(), [&](int i, int j) {
-        return s[i] < s[j];
-    });
-    for (int i = 0; i < n; i++) id[p[i]] = i;
-    while (q--) {
-        int k, L;
-        cin >> k >> L;
-        vector<int> x(k);
-        for (int i = 0; i < k; i++) {
-            cin >> x[i];
-            x[i]--;
-        }
-        sort(x.begin(), x.end(), [&](int i, int j) {
-            return id[i] < id[j];
-        });
-        int res = 0;
-        for (int i = 0; i < k - L + 1; i++) {
-            int cost = LCP(x[i], x[i + L - 1]);
-            int cL = 0, cR = 0;
-            if (i > 0) cL = LCP(x[i - 1], x[i]);
-            if (i + L < k) cR = LCP(x[i], x[i + L]);
-            res += cost - min(cost, max(cL, cR));
-        }
-        cout << res << '\n';
+        cout << res,EL;
     }
 }
 
-void solve() {
-    int T;
-    //cin >> T;
-    T = 1;
-    int TestCase = 0;
-    while (T--) {
-        TestCase += 1;
-        //cout << "Case #" << TestCase << ":" << ' ';
-        ///cout << "Case #" << TestCase << '\n';
-        sol();
+int main(){
+    io_faster
+    if(fopen(PROB".inp", "r")){
+        freopen(PROB".inp","r",stdin);
+        freopen(PROB".out","w",stdout);
     }
+    int t = 1;
+//    cin >> t;
+    while (t--) solve();
 }
-int main(int argc,char *argv[]) {
-    //srand(time(nullptr));
-    ios_base::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
-    open_file();
-    solve();
-}
+
